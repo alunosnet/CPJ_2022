8368c46088c94fcf9a7c8a5da4753fab67d82f41
Paulo Ferreira <alunosnet@gmail.com>
2021-10-13 20:31:14 +0100

    start of CPJ_2022

---
+++ Assets/Scripts/moverplayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverplayer : MonoBehaviour
+{
+    [SerializeField] float velocidadeAndar = 5;
+    [SerializeField] float velocidadeRodar = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float andar = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * andar * velocidadeAndar*Time.deltaTime);
+
+        float rodar = Input.GetAxis("Horizontal");
+        transform.Rotate(transform.up * rodar * velocidadeRodar * Time.deltaTime);
+        //TODO: Rodar com o rato e as teclas para andar para o lado
+    }
+}
