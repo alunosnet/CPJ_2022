1a7df2f5323d254c1d7e72e3a1d84f095d4bdb38
alunosNET <alunosnet@gmail.com>
2021-11-10 16:45:11 +0000

    Flecha explosiva

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    [SerializeField] float RaioExplosao=10.0f;
+    [SerializeField] float ForcaExplosao=10.0f;
+
+    [SerializeField] float TempoExplodir=-1f;
+    //TODO: Dano da explosão em NPC, SOM, Efeitos de particulas
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   private void OnTriggerEnter(Collider other){
+       Explodir();
+   }
+    private void OnCollisionEnter(Collision collision){
+        Explodir();
+    }
+    void Explodir(){
+        Debug.Log("BOOM");
+        Vector3 posicaoExplosao=transform.position;
+        Collider[] colliders=Physics.OverlapSphere(posicaoExplosao,RaioExplosao);
+        foreach(Collider obj in colliders){
+            Rigidbody rb=obj.GetComponent<Rigidbody>();
+            if(rb!=null)
+                rb.AddExplosionForce(ForcaExplosao,posicaoExplosao,RaioExplosao,3.0f);
+
+            //TODO: se tiver vida => tirar vida
+        }
+
+        //TODO: SOM e efeito de particulas
+
+        Destroy(this.gameObject);
+    }
+}
